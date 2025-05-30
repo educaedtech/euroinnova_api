a74a743c681abf024ea88d4da3346b99ea4678b8
Ismael Pérez Díaz <ismael.perez8411@gmail.com>
2025-05-30 01:01:32 -0400

    adjust redis

--- src/config/queue.config.ts
+++ src/config/queue.config.ts
@@ -1,9 +1,12 @@
 // src/config/queue.config.ts
+// console.log(process.env.REDIS_HOST)
 export const queueConfig = {
   redis: {
+    // redisUrl: process.env.REDIS_URL ?? 'redis://localhost:6379',
     host: process.env.REDIS_HOST ?? 'localhost',
     port: parseInt(process.env.REDIS_PORT ?? '6379'),
     password: process.env.REDIS_PASSWORD ?? undefined,
+    username: process.env.REDIS_USERNAME ?? undefined,
   },
   limiter: {
     max: parseInt(process.env.QUEUE_LIMIT_MAX ?? '100'),
