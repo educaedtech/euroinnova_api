15c434eeb123dc199a793594845ddcea323a64e8
Ismael Pérez Díaz <ismael.perez8411@gmail.com>
2025-05-30 01:08:41 -0400

    adjust redis config

--- src/config/queue.config.ts
+++ src/config/queue.config.ts
@@ -1,12 +1,13 @@
 // src/config/queue.config.ts
-// console.log(process.env.REDIS_HOST)
+console.log(process.env.REDIS_HOST)
 export const queueConfig = {
   redis: {
     // redisUrl: process.env.REDIS_URL ?? 'redis://localhost:6379',
-    host: process.env.REDIS_HOST ?? 'localhost',
-    port: parseInt(process.env.REDIS_PORT ?? '6379'),
-    password: process.env.REDIS_PASSWORD ?? undefined,
-    username: process.env.REDIS_USERNAME ?? undefined,
+    host: 'oregon-keyvalue.render.com',//process.env.REDIS_HOST ?? 'localhost',
+    port: 6379,//parseInt(process.env.REDIS_PORT ?? '6379'),
+    password: 'eRwcpsGD3MP2Nak9JoEX8NPS0Qw9B98B',//rocess.env.REDIS_PASSWORD ?? undefined,
+    username: 'red-d0sahnvdiees73a7b2s0',//process.env.REDIS_USERNAME ?? undefined,
+    tls: {}
   },
   limiter: {
     max: parseInt(process.env.QUEUE_LIMIT_MAX ?? '100'),
