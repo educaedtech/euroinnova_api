5fa24c3eaba4f9de8191cc00ab5c2e8a015f9141
Ismael Pérez Díaz <ismael.perez8411@gmail.com>
2025-05-30 01:12:51 -0400

    adj

--- src/config/queue.config.ts
+++ src/config/queue.config.ts
@@ -2,12 +2,14 @@
 console.log(process.env.REDIS_HOST)
 export const queueConfig = {
   redis: {
+    host: 'localhost',
+    port: 6379
     // redisUrl: process.env.REDIS_URL ?? 'redis://localhost:6379',
-    host: 'oregon-keyvalue.render.com',//process.env.REDIS_HOST ?? 'localhost',
-    port: 6379,//parseInt(process.env.REDIS_PORT ?? '6379'),
-    password: 'eRwcpsGD3MP2Nak9JoEX8NPS0Qw9B98B',//rocess.env.REDIS_PASSWORD ?? undefined,
-    username: 'red-d0sahnvdiees73a7b2s0',//process.env.REDIS_USERNAME ?? undefined,
-    tls: {}
+    // host: 'oregon-keyvalue.render.com',//process.env.REDIS_HOST ?? 'localhost',
+    // port: 6379,//parseInt(process.env.REDIS_PORT ?? '6379'),
+    // password: 'eRwcpsGD3MP2Nak9JoEX8NPS0Qw9B98B',//rocess.env.REDIS_PASSWORD ?? undefined,
+    // username: 'red-d0sahnvdiees73a7b2s0',//process.env.REDIS_USERNAME ?? undefined,
+    // tls: {}
   },
   limiter: {
     max: parseInt(process.env.QUEUE_LIMIT_MAX ?? '100'),
